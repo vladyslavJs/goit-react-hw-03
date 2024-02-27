2854ba4a93709a90b1007ed0820712d859802168
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-02-27 16:35:47 +0100

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 import { useEffect, useState } from 'react';
-import './App.css';
+import css from './App.module.css';
+import { FaAddressBook } from "react-icons/fa";
 
 import ContactList from '../ContactList/ContactList';
 import SearchBox from '../SearchBox/Searchbox';
@@ -10,8 +11,6 @@ import ContactForm from '../ContactForm/ContactForm';
 const userContacts = [
   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
 ];
 
 const localStorageContacts = () => {
@@ -47,11 +46,15 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Phonebook</h1>
+    <div className={css.container}>
+      <h1 className={css.title}>Phonebook <FaAddressBook /></h1>
+      <SearchBox value={filter} onFilter={setFilter} />
+      <hr />
       <ContactForm onAdd={addContact} />
-      <SearchBox value={filter} onFilter={setFilter}/>
+      <hr />
+      <h2 className={css.contactTitle}>Contacts:</h2>
       <ContactList contacts={filterContacts} onDelete={deleteContact} />
+      <hr />
     </div>
   )
 }
